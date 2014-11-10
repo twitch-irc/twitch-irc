d96c5407e05bcf4786f134c0fa6069bc5b6af2fd
Schmoopiie <Schmoopiie@gmail.com>
2014-11-09 19:15:53 -0500

    Some beta tests.

---
+++ beta/beta.js
@@ -0,0 +1,73 @@
+var irc = require('twitch-irc');
+
+var client = new irc.client({
+    options: {
+        debug: false,
+        debugIgnore: ['ping', 'chat', 'action'],
+        logging: false,
+        tc: 3
+    },
+    channels: ['']
+});
+
+client.connect();
+
+/**
+ * Change the TWITCHCLIENT value using RAW message.
+ */
+client.addListener('connected', function () {
+    client.raw('TWITCHCLIENT 1');
+});
+
+/**
+ * Calling the Twitch API to get a list of chatters in a channel.
+ */
+client.api.chatters('schmoopiie', function(err, response) {
+    if (!err) {
+        console.log(' ');
+        console.log('--- CHATTERS ---');
+        console.log(' ');
+        console.log(response);
+    }
+});
+
+/**
+ * Calling the Twitch API to get the badges of a channel.
+ */
+client.api.badges('schmoopiie', function(err, response) {
+    if (!err) {
+        console.log(' ');
+        console.log('--- BADGES ---');
+        console.log(' ');
+        console.log(response);
+    }
+});
+
+/**
+ * Calling the Twitch API to get a channel object.
+ */
+client.api.channels('schmoopiie', function(err, response) {
+    if (!err) {
+        console.log(' ');
+        console.log('--- CHANNEL OBJECT ---');
+        console.log(' ');
+        console.log(response);
+    }
+});
+
+/**
+ * Cron job example.
+ *
+ * It starts after 3 seconds and then stop 10 seconds later.
+ */
+var cronJob = client.cron('* * * * * *', function() {
+    console.log('You will see this message every second for 10 seconds.');
+});
+
+setTimeout(function(){
+    cronJob.start();
+}, 3000);
+
+setTimeout(function(){
+    cronJob.stop();
+}, 13000);
