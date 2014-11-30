1a8b95f7d9279a1a8cecec5cd58d3ec0d2e2e6a6
Schmoopiie <Schmoopiie@gmail.com>
2014-11-29 22:06:20 -0500

    Added levenshtein.

---
+++ library/utils/levenshtein.js
@@ -0,0 +1,101 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Schmoopiie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * The levenshtein distance is a string metric for measuring the difference between two sequences.
+ *
+ * @type {{utils: {levenshtein: levenshtein}}}
+ */
+module.exports = {
+    utils: {
+        levenshtein: function(s1, s2, caseSensitive) {
+            var cost_ins = 1;
+            var cost_rep = 1;
+            var cost_del = 1;
+            caseSensitive = typeof caseSensitive !== 'undefined' ? caseSensitive : false;
+
+            if (!caseSensitive) {
+                s1 = s1.toLowerCase();
+                s2 = s2.toLowerCase();
+            }
+
+            if (s1 == s2) { return 0; }
+
+            var l1 = s1.length;
+            var l2 = s2.length;
+
+            if (l1 === 0) { return l2 * cost_ins; }
+            if (l2 === 0) { return l1 * cost_del; }
+
+            var split = false;
+            try {
+                split = !('0')[0];
+            } catch (e) {
+                split = true;
+            }
+            if (split) {
+                s1 = s1.split('');
+                s2 = s2.split('');
+            }
+
+            var p1 = new Array(l2 + 1);
+            var p2 = new Array(l2 + 1);
+
+            var i1, i2, c0, c1, c2, tmp;
+
+            for (i2 = 0; i2 <= l2; i2++) {
+                p1[i2] = i2 * cost_ins;
+            }
+
+            for (i1 = 0; i1 < l1; i1++) {
+                p2[0] = p1[0] + cost_del;
+
+                for (i2 = 0; i2 < l2; i2++) {
+                    c0 = p1[i2] + ((s1[i1] == s2[i2]) ? 0 : cost_rep);
+                    c1 = p1[i2 + 1] + cost_del;
+
+                    if (c1 < c0) {
+                        c0 = c1;
+                    }
+
+                    c2 = p2[i2] + cost_ins;
+
+                    if (c2 < c0) {
+                        c0 = c2;
+                    }
+
+                    p2[i2 + 1] = c0;
+                }
+
+                tmp = p1;
+                p1 = p2;
+                p2 = tmp;
+            }
+
+            c0 = p1[l2];
+
+            return c0;
+        }
+    }
+};
